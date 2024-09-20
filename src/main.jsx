5283d3fcf756ae0771d9a2f2d67b068154cb5e7b
Gorra.js <gorrahabla1234@gmail.com>
2024-09-20 10:02:28 -0300

    Primera API de prueba, lista para usar

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import axios from 'axios';
+
+const options = {
+  method: 'GET',
+  url: 'https://online-movie-database.p.rapidapi.com/auto-complete',
+  params: {q: 'john wick'},
+  headers: {
+    'x-rapidapi-key': 'cb71281a97msh036212d3e3bc848p18e3e2jsn541ec39839d0',
+    'x-rapidapi-host': 'online-movie-database.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
